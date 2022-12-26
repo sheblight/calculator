d75be8f8a170a077a49b2f2cf49e1d4612dee949
sheblight <sheblight@gmail.com>
2022-12-26 13:39:05 -0500

    Remove old logic for update display

--- script.js
+++ script.js
@@ -22,115 +22,17 @@ function modulo(a,b) {
     return round(a%b);
 }
 
-let num1 = "";
-let num2 = "";
-let operator = "";
-let isSecondOperand = false;
-let previousSelectedOperator = null;
+const display = document.querySelector("p");
+let num1 = null;
+let num2 = null;
+let operatorMethod = null;
 
-function operate(operatorMethod) {
-    return operatorMethod(Number(num1), Number(num2));
+// pre-condition: num1 and num2 is an integer and operatorMethod is valid method
+function operate() {
+    return operatorMethod(num1, num2);
 }
 
-
 function updateDisplay() {
-    const display = document.querySelector("p");
-
-    switch (this.value) {
-        case "clear":
-            isSecondOperand = false;
-            num1 = "";
-            num2 = "";
-            display.textContent = "0";
-            // turn off highlighted button after evaluation
-            if (previousSelectedOperator != null) {
-                let backgroundColor = getComputedStyle(this).getPropertyValue("--button-right-color");
-                previousSelectedOperator.style.backgroundColor = backgroundColor;
-                previousSelectedOperator = null;
-            }
-            break;
-        case "negative":
-            if (display.textContent == "0" || display.textContent == "0.") break;
-            display.textContent = display.textContent.includes("-") ? display.textContent.replace("-","") : `-${display.textContent}`;
-            break;
-        case ".":
-            if (display.textContent.includes(".")) break;
-            display.textContent += ".";
-            break;
-        case "=":
-            if (!isSecondOperand) break;
-            num2 = display.textContent;
-            let result = 0;
-            switch (operator) {
-                case "%":
-                    result = operate(modulo);
-                    break;
-                case "+":
-                    result = operate(add);
-                    break;
-                case "-":
-                    result = operate(subtract);
-                    break;
-                case "*":
-                    result = operate(multiply);
-                    break;
-                case "/":
-                    result = operate(divide);
-                    break;
-                default:
-                    break;
-            }
-            
-            // turn off highlighted button after evaluation
-            if (previousSelectedOperator != null) {
-                let backgroundColor = getComputedStyle(this).getPropertyValue("--button-right-color");
-                previousSelectedOperator.style.backgroundColor = backgroundColor;
-                previousSelectedOperator = null;
-            }
-
-            display.textContent = result.toString().slice(0, 10);
-            isSecondOperand = false;
-
-            // if result is infinity, alert and reset to zero
-            if (display.textContent == "Infinity") {
-                alert("Cannot divide by 0!");
-                display.textContent = "0";
-            }
-            break;
-        default:
-            if (Number.isNaN(Number(this.value))) {
-                num1 = display.textContent;
-                operator = this.value;
-                isSecondOperand = true;
-                
-                if (previousSelectedOperator != null) {
-                    let backgroundColor = getComputedStyle(this).getPropertyValue("--button-right-color");
-                    previousSelectedOperator.style.backgroundColor = backgroundColor;
-                    previousSelectedOperator = null;
-                }
-                let highlightColor = getComputedStyle(this).getPropertyValue("--button-color-highlight");
-                this.style.backgroundColor = highlightColor;
-                previousSelectedOperator = this;
-                
-            }
-            else if (isSecondOperand) {
-                if (display.textContent == num1) {
-                    display.textContent = this.value;
-                }
-                else if (display.textContent.length <= 9) {
-                    display.textContent += this.value;
-                }
-            }
-            else {
-                if (display.textContent == "0") {
-                    display.textContent = this.value;
-                }
-                else if (display.textContent.length <= 9) {
-                    display.textContent += this.value;
-                }
-            }
-            break;
-    }
 }
 
 const buttons = document.querySelectorAll("button");
